aafe13023d091ff9491a1dd71f37438da36cac8d
Leo-de-Riv3r <leoguti182@gmail.com>
2025-11-20 22:10:58 -0300

    falta reducir los logs que se muestran

--- agregador/src/main/java/ar/edu/utn/frba/dds/models/entities/utils/AbstractConverter.java
+++ agregador/src/main/java/ar/edu/utn/frba/dds/models/entities/utils/AbstractConverter.java
@@ -2,11 +2,11 @@ package ar.edu.utn.frba.dds.models.entities.utils;
 
 import java.util.List;
 
-public abstract class AbstractConverter<E,D,O> {
+public abstract class AbstractConverter<E, D, O> {
   public abstract E fromDto(D dto);
   public abstract O fromEntity(E entity);
 
-  public List<O> fromEntity(List<E> entities){
+  public List<O> fromEntity(List<E> entities) {
     if (entities == null) return null;
     return entities.stream().map(ent -> fromEntity(ent)).toList();
   }
