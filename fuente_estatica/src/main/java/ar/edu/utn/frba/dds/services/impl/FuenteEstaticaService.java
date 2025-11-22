c2be9c30dd013a6f7fb6d1737cd37e64b87aeb80
Leo-de-Riv3r <leoguti182@gmail.com>
2025-11-22 14:33:21 -0300

    modifico fuente estatica

--- fuente_estatica/src/main/java/ar/edu/utn/frba/dds/services/impl/FuenteEstaticaService.java
+++ fuente_estatica/src/main/java/ar/edu/utn/frba/dds/services/impl/FuenteEstaticaService.java
@@ -35,7 +35,7 @@ public class FuenteEstaticaService implements IFuenteEstaticaService {
     Fuente fuente = fuenteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Fuente no encontrada"));
     List<Hecho> hechos = fuente.getHechos();
     if (page < 1) page = 1;
-    if (per_page > 10) per_page = 10;
+    if (per_page > 2000) per_page = 2000;
     if (page < 1) page = 1;
     Integer total = hechos.size();
     Integer inicio = (page - 1) * per_page;
